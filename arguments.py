e1cc2eeb3503b8872b0e310648c99f52cc2d0021
DEVESHTARASIA <deveshtarasia@gmail.com>
2022-12-12 22:40:59 -0500

    Training Image Added

--- arguments.py
+++
@@ -1,7 +0,0 @@
-import sys
-import argparse
-
-print("No. of arguments passed: ",len(sys.argv))
-print("First argument is the name of the script: ",sys.argv[0])
-print("Can this be a string? ",type(sys.argv[1]))
-print("Is it an integer",type(sys.argv[2])) #Everything is understood as string
